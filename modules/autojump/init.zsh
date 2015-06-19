34b0e3bec3424f949a9013e9926ec769eb640ebb
Gabriel Adomnicai <gabriel@slideshare.com>
2015-06-19 10:14:31 -0700

    Autojump and fzf fixes

--- modules/autojump/init.zsh
+++ modules/autojump/init.zsh
@@ -1,12 +1,17 @@
+local autojump=''
+
 if [ -f /opt/local/etc/profile.d/autojump.sh ]; then
     autojump="/opt/local/etc/profile.d/autojump.sh"
 fi
+
 if [ -f /etc/profile.d/autojump.sh ]; then
     autojump="/etc/profile.d/autojump.sh"
 fi
+
 if [[ -f /root/.autojump/etc/profile.d/autojump.sh ]]; then
     autojump="/root/.autojump/etc/profile.d/autojump.sh"
 fi
+
 if hash brew 2>/dev/null; then
     if [[ -s `brew --prefix`/etc/autojump.sh ]]; then
         autojump="`brew --prefix`/etc/autojump.sh"
